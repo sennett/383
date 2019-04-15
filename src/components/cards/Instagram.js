c04f5d0d2574d4c7b4b58a2ce339c61e01da84b2
sennett <qaplwsokedijrfuhtg6>
2019-04-15 23:38:27 +0200

    instagram hashtags linking

--- src/components/cards/Instagram.js
+++ src/components/cards/Instagram.js
@@ -2,14 +2,20 @@ import GenericCard from "./GenericCard";
 import React from "react";
 import './Instagram.css'
 
+import { autoLinkHashtags } from 'twitter-text'
+
 const Instagram = ({item}) => {
     const tagContent = "📷";
     item.image.medium = "https://placekitten.com/g/200/400";
+    const caption = autoLinkHashtags(item.caption, {
+        hashtagUrlBase: 'https://www.instagram.com/explore/tags/',
+        targetBlank: true
+    });
     return (
         <GenericCard tagContent={tagContent}>
             <img alt={item.caption} className="instagram-card__image" src={item.image.medium}/>
             <p className="instagram-card__username">{item.user.username}</p>
-            <p className="instagram-card__caption">{item.caption}</p>
+            <p className="instagram-card__caption" dangerouslySetInnerHTML={{ __html: caption}} />
         </GenericCard>
     );
 };
