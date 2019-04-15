79c54b353e072ada7bd1564b301ba292bbdf943a
sennett <qaplwsokedijrfuhtg6>
2019-04-15 23:26:33 +0200

    linking tweets

--- src/components/cards/Twitter.js
+++ src/components/cards/Twitter.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import GenericCard from "./GenericCard";
 import './Twitter.css';
-
-import decodeHtml from './decodeHtml'
+import { autoLink, htmlEscape } from 'twitter-text'
 
 const Twitter = ({item}) => {
     const tagContent = "🐦";
+    let tweet = autoLink(htmlEscape(item.tweet), {targetBlank: true});
+    // tweet =
     return (
         <GenericCard tagContent={tagContent}>
             <div className="twitter-card__author">{item.user.username}</div>
-            <div className="twitter-card__tweet">{decodeHtml(item.tweet)}</div>
+            <div className="twitter-card__tweet" dangerouslySetInnerHTML={{ __html: tweet}} />
         </GenericCard>
     );
 };
--- src/components/cards/decodeHtml.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-// https://stackoverflow.com/a/42361869/614523
-
-const renderHTML = (rawHTML) => React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
-
-export default renderHTML
